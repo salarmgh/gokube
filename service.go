eb97cf72a79e522c812dae8e65898a7fa228f1b1
Salar Moghaddam <salarmgh@gmail.com>
2020-08-11 13:13:59 +0430

    Active env image

--- service.go
+++ service.go
@@ -31,10 +31,29 @@ func (k *Kube) GetService(name string, namespace string, selector map[string]str
 	return &svcs.Items[0], nil
 }
 
+func (k *Kube) GetDeployment(name string, namespace string, selector map[string]string) (*apiv1.Service, error) {
+	set := labels.Set(selector)
+	listOptions := metav1.ListOptions{LabelSelector: set.AsSelector().String()}
+	deployments, err := k.clientset.CoreV1().Deployments(namespace).List(listOptions)
+	if err != nil {
+		return nil, err
+	}
+	return &deployments.Items[0], nil
+}
+
 func (k *Kube) GetActiveEnv(deployment string, namespace string) (string, error) {
 	svc, err := k.GetService(fmt.Sprintf("%s-active", deployment), namespace, map[string]string{"app.kubernetes.io/instance": deployment})
 	if err != nil {
 		return "", err
 	}
 	return svc.Spec.Selector["app.env"], nil
+}
+
+func (k *Kube) GetActiveEnvImage(deployment string, namespace string) (string, error) {
+	env, err := k.GetActiveEnv(deployment, namespace)
+	if err != nil {
+		return "", err
+	}
+	deployment, err := getDeployment(deployment, namespaces, map[string]string{"app.env": env})
+	return deployment, nil
 }
